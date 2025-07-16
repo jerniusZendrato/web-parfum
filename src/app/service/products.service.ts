61d90ee8d3aa03b66cfa4fcf83ad659c395ecc90
jerniusZendrato <jernius0909@gmail.com>
2025-07-16 16:44:15 +0700

    update gambar dan bisa muncul di item keranjang

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -10,7 +10,7 @@ export class ProductsService {
   products = [
     {
       id: '001',
-      img: 'https://images.unsplash.com/photo-1626178793922-eae75f37880e?auto=format&fit=crop&w=500&q=80',
+      img: 'https://picsum.photos/200',
       name: "New Gabriele chanel paris man's perfume",
       desc: 'An ode to masculine freedom expressed in an aromatic woody fragrance.',
       tipe: 1,
@@ -18,7 +18,7 @@ export class ProductsService {
     },
     {
       id: '002',
-      img: 'https://images.unsplash.com/photo-1585155774081-b55f53b59610?auto=format&fit=crop&w=500&q=80',
+      img: 'https://picsum.photos/200',
       name: "New Gabriele chanel paris woman's perfume",
       desc: '',
       tipe: 1,
@@ -26,7 +26,7 @@ export class ProductsService {
     },
     {
       id: '003',
-      img: 'https://images.unsplash.com/photo-1610919265408-1968f354d7c8?auto=format&fit=crop&w=500&q=80',
+      img: 'https://picsum.photos/200',
       name: "Woman's Coco Perfume",
       desc: 'The aroma of love and captivating memory.',
       tipe: 1,
@@ -34,18 +34,18 @@ export class ProductsService {
     },
     {
       id: '003',
-      img: 'https://images.unsplash.com/photo-1597764699516-8fd6a3aa504d?auto=format&fit=crop&w=500&q=80',
+      img: 'https://picsum.photos/200',
       name: 'Velvet Rose Perfume',
       desc: 'Floral elegance meets modern luxury.',
-      tipe: 0,
+      tipe: 1,
       price: 240,
     },
     {
       id: '003',
-      img: 'https://images.unsplash.com/photo-1603808033190-f09be539c445?auto=format&fit=crop&w=500&q=80',
+      img: 'https://picsum.photos/200',
       name: 'Classic Oud',
       desc: 'Intense and unforgettable oriental scent.',
-      tipe: 0,
+      tipe: 1,
       price: 260,
     },
   ];
