0f530bb0eff723e21781c3db0c25bf3387eb6f6b
jerniusZendrato <jernius0909@gmail.com>
2025-07-17 14:55:32 +0700

    update data product

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -14,7 +14,7 @@ export class ProductsService {
       name: "New Gabriele chanel paris man's perfume",
       desc: 'An ode to masculine freedom expressed in an aromatic woody fragrance.',
       tipe: 1,
-      price: 200,
+      price: 200000,
     },
     {
       id: '002',
@@ -22,7 +22,7 @@ export class ProductsService {
       name: "New Gabriele chanel paris woman's perfume",
       desc: '',
       tipe: 1,
-      price: 210,
+      price: 210000,
     },
     {
       id: '003',
@@ -30,7 +30,7 @@ export class ProductsService {
       name: "Woman's Coco Perfume",
       desc: 'The aroma of love and captivating memory.',
       tipe: 1,
-      price: 185,
+      price: 185000,
     },
     {
       id: '003',
@@ -38,7 +38,7 @@ export class ProductsService {
       name: 'Velvet Rose Perfume',
       desc: 'Floral elegance meets modern luxury.',
       tipe: 1,
-      price: 240,
+      price: 240000,
     },
     {
       id: '003',
@@ -46,7 +46,7 @@ export class ProductsService {
       name: 'Classic Oud',
       desc: 'Intense and unforgettable oriental scent.',
       tipe: 1,
-      price: 260,
+      price: 260000,
     },
   ];
 
