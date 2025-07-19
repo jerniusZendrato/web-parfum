a45f9a0491898fb3c2cadde68a25f7105fd83fe9
jerniusZendrato <jernius0909@gmail.com>
2025-07-19 13:19:16 +0700

    buat loading awal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PreloaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'parfum-web';
+  loading = true;
+
+  ngOnInit() {
+  setTimeout(() => {
+    this.loading = false;
+  }, 2500); // 2.5 detik
+}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+          shimmer: 'shimmer 1.5s infinite linear',
+    mist: 'mist 2.5s ease-in-out infinite',
+      },
+      keyframes: {
+        shimmer: {
+      '0%': { transform: 'translateX(-100%)' },
+      '100%': { transform: 'translateX(100%)' },
+    },
+    mist: {
+      '0%': { transform: 'translateY(0)', opacity: '0.4' },
+      '100%': { transform: 'translateY(-20px)', opacity: '0' },
+    },
+      },
+    },
   },
   plugins: [],
 }
